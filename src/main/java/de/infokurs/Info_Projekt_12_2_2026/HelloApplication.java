8c71cba97d5b6b54d24d2e9fd4e894fe67aac455
TheGreenBotWasTaken <tgb@alpenjodel.de>
2026-04-28 09:37:55 +0200

    jfx added

---
+++ src/main/java/de/infokurs/Info_Projekt_12_2_2026/HelloApplication.java
@@ -0,0 +1,26 @@
+package de.infokurs.Info_Projekt_12_2_2026;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/de/infokurs/Info_Projekt_12_2_2026/test.fxml"));
+
+        Scene scene = new Scene(root, 300, 275);
+        stage.setScene(scene);
+
+
+
+        stage.getIcons().add(new Image(getClass().getResource("/assets/icon.png").toExternalForm()));
+        stage.setTitle(Properties.NAME);
+        stage.show();
+    }
+}
---
+++ src/main/java/de/infokurs/Info_Projekt_12_2_2026/Properties.java
@@ -0,0 +1,7 @@
+package de.infokurs.Info_Projekt_12_2_2026;
+
+public class Properties {
+    public static final float VERSION = 1.0f;
+    public static final String NAME = "UNICORN CLICKING SIMULATOR DELUXE ULTIMATE V. " + (2000 + VERSION);
+
+}
